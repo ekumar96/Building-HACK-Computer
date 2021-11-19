91a671456f21f12e968372909cf27e4cb086fb64
Eshan Kumar <36972518+ekumar96@users.noreply.github.com>
2021-11-19 12:03:47 -0500

    Add files via upload

---
+++ Project4/Fill.asm
@@ -0,0 +1,82 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Fill.asm
+
+// Runs an infinite loop that listens to the keyboard input.
+// When a key is pressed (any key), the program blackens the screen,
+// i.e. writes "black" in every pixel;
+// the screen should remain fully black as long as the key is pressed. 
+// When no key is pressed, the program clears the screen, i.e. writes
+// "white" in every pixel;
+// the screen should remain fully clear as long as no key is pressed.
+
+// Put your code here.
+
+    @SCREEN
+    D=A
+    @addr
+    M=D
+    
+    @24576
+    D=A
+    @lastscreenword
+    M=D
+
+(PROBE)
+    @KBD
+    D=M
+    
+    @FLOODSCREEN
+    D; JNE
+    @DRAINSCREEN
+    D; JEQ
+    
+
+(FLOODSCREEN)
+  (LOOP1)
+    @lastscreenword
+    D=M
+    @addr
+    D=D-M
+    @STOP
+    D; JLE
+
+    @addr
+    A=M
+    M=-1
+
+    @addr
+    M=M+1
+
+    @LOOP1
+    0; JMP
+
+(DRAINSCREEN)
+  (LOOP2)
+    @lastscreenword
+    D=M
+    @addr
+    D=D-M
+    @STOP
+    D; JLE
+
+    @addr
+    A=M
+    M=0
+
+    @addr
+    M=M+1
+
+    @LOOP2
+    0; JMP
+
+(STOP)
+    @SCREEN
+    D=A
+    @addr
+    M=D
+    @PROBE
+    0; JMP
+    
+    
---
+++ Project4/Mult.asm
@@ -0,0 +1,64 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Mult.asm
+
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+//
+// This program only needs to handle arguments that satisfy
+// R0 >= 0, R1 >= 0, and R0*R1 < 32768.
+
+// Put your code here.
+
+    //variable initialization
+    @i
+    M=0      //i=0
+    
+    @R0
+    D=M
+    @multi1
+    M=D      //multi1 = R0
+    
+    @R1
+    D=M
+    @multi2
+    M=D      //multi2 = R1
+
+    @product
+    M=0      //product and R2 = 0
+    @R2
+    M=0
+
+(LOOP) //for loop
+    @multi1
+    D=M
+    @i
+    D=D-M
+    @STOP
+    D; JLE   //checks if multi1 - i <= 0, if so breaks loop
+
+    @multi2
+    D=M
+    @product
+    M=D+M    //product = product + multi2
+
+    @i
+    M=M+1   //iterating counter variable
+
+    @LOOP
+    0; JMP
+
+    
+(STOP) //what to do after breaking for loop
+    @product
+    D=M
+    @R2
+    M=D
+
+(END) //infinite loop at end of program
+    @END
+    0; JMP
+
+    
+
